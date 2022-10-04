ccd52c2217ae42e915a1522d612a084f9d0cbb1e
Jaidevch <chakkajaidev@gmail.com>
2022-10-04 17:57:34 -0500

    Update Training.py

--- Training.py
+++ Training.py
@@ -1,1 +1,18 @@
+#This is the code for identifying the small number than 10
+def is_small(x):
+    if (x < 10):
+        return (True)
+    else:
+        return (False)
+    return ()
+print(is_small(1))
+
+#This is the password checker program
+def check(x):
+    if (x == "secret"):
+        return (True)
+    else:
+        return (False)
+    return (x)
+check("secret")
 
